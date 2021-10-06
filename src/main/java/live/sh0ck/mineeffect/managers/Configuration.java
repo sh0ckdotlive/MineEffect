a6170dba400a403424667a0df0a49003ee258334
sh0ckR6 <sh0ck.04@outlook.com>
2021-10-05 20:40:24 -0400

    Bring over config system from sh0ckR6/AchievementBorder

---
+++ src/main/java/live/sh0ck/mineeffect/managers/Configuration.java
@@ -0,0 +1,50 @@
+package live.sh0ck.mineeffect.managers;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+/**
+ * Represents a {@link YamlConfiguration}, it's corresponding {@link File}, and the name of the configuration
+ *
+ * @author sh0ckR6
+ * @since 2021.1010.1
+ */
+public class Configuration {
+  
+  /**
+   * The current {@link YamlConfiguration}
+   *
+   * @since 2021.1010.1
+   */
+  public YamlConfiguration yamlConfig;
+  
+  /**
+   * The current {@link File}
+   *
+   * @since 2021.1010.1
+   */
+  public File file;
+  
+  /**
+   * This configuration's name
+   *
+   * @since 2021.1010.1
+   */
+  public String name;
+  
+  /**
+   * Constructor to create a new Configuration
+   *
+   * @param yamlConfig The {@link YamlConfiguration} this will represent
+   * @param file The {@link File} this will represent
+   * @param name The name of this configuration
+   * @author sh0ckR6
+   * @since 2021.1010.1
+   */
+  public Configuration(YamlConfiguration yamlConfig, File file, String name) {
+    this.yamlConfig = yamlConfig;
+    this.file = file;
+    this.name = name;
+  }
+}
