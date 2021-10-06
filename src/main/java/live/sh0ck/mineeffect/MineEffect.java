60759ce7b901380fd99c1f8109d8872ea501c710
sh0ckR6 <sh0ck.04@outlook.com>
2021-10-05 23:39:23 -0400

    Add new default config options
    New defaults for config.yml:
    effects.amplifier.min: Minimum amplifier when randomizing
    effects.amplifier.max: Maximum amplifier when randomizing
    effects.duration.min: Minimum duration when randomizing
    effects.duration.max: Maximum duration when randomizng

--- src/main/java/live/sh0ck/mineeffect/MineEffect.java
+++ src/main/java/live/sh0ck/mineeffect/MineEffect.java
@@ -1,6 +1,8 @@
 package live.sh0ck.mineeffect;
 
 import live.sh0ck.mineeffect.handlers.EffectHandler;
+import live.sh0ck.mineeffect.managers.ConfigManager;
+import live.sh0ck.mineeffect.managers.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MineEffect extends JavaPlugin {
@@ -8,9 +10,28 @@ public class MineEffect extends JavaPlugin {
   @Override
   public void onEnable() {
     registerHandlers();
+    registerConfigs();
   }
   
   private void registerHandlers() {
     new EffectHandler(this);
+  }
+  
+  private void registerConfigs() {
+    // Load and cache all configs
+    ConfigManager.loadAllConfigs(this);
+    
+    // Setup config.yml defaults
+    ConfigManager.createIfNotPresent("config", this);
+    Configuration config = ConfigManager.getConfig("config");
+    config.yamlConfig.addDefault("effects.amplifier.min", 0);
+    config.yamlConfig.addDefault("effects.amplifier.max", 9);
+    config.yamlConfig.addDefault("effects.duration.min", 0);
+    config.yamlConfig.addDefault("effects.duration.max", 15);
+    config.yamlConfig.options().copyDefaults(true);
+    ConfigManager.saveConfig(config);
+    
+    // Reload after setting defaults
+    ConfigManager.reloadConfigs(this);
   }
 }
